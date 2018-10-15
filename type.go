c5c1570777ad7f69b4fb80f069e53f5bc4b638ce
Julian Matschinske <julian.matschinske@bitspark.de>
2018-10-15 23:06:55 +0200

    Experimental expansion of types with map templates

--- type.go
+++ type.go
@@ -6,21 +6,35 @@ import (
 
 // Type defines structure for data.
 type Type struct {
+	// The name of the role this type has in its context.
+	// Can make use of properties.
+	Name string `json:"name" yaml:"name"`
+
 	// Type is either "stream", "map", "generic", "reference" or a type the target system understands.
 	// Normally, the following types should be available: "string", "number", "boolean", "trigger".
+	// Cannot (!) make use of properties.
 	Type string `json:"type" yaml:"type"`
 
 	/* === REFERENCE + SPECIFICATION (only in case Type == "reference") === */
 
 	// Reference is the name of a referenced type.
+	// Cannot (!) make use of properties.
 	Reference string `json:"reference,omitempty" yaml:"reference,omitempty"`
 
 	// Generics is a specification of the generics inside a referenced type.
 	Generics Generics `json:"generics,omitempty" yaml:"generics,omitempty"`
 
+	// Values is a specification of the properties inside a referenced type.
+	Values Values `json:"values,omitempty" yaml:"values,omitempty"`
+
 	/* === DEFINITION === */
 
+	// Properties of this type and its subtypes.
+	// Cannot (!) make use of properties.
+	Properties Properties `json:"properties" yaml:"properties"`
+
 	// Description of this type in natural language (English).
+	// Can make use of properties.
 	Description string `json:"description" yaml:"description"`
 
 	// Stream is the underlying type for the stream.
@@ -29,40 +43,53 @@ type Type struct {
 
 	// Map is a map of underlying types.
 	// It is ignored in case Type is not "map".
-	Map Map `json:"map,omitempty" yaml:"map,omitempty"`
+	Map []*Type `json:"map,omitempty" yaml:"map,omitempty"`
+
+	// MapTemplate specifies a map being built from a property.
+	// It is ignored in case Type is not "map".
+	MapTemplate MapTemplate `json:"mapTemplate,omitempty" yaml:"mapTemplate,omitempty"`
 
 	// Generic is a placeholder for an arbitrary type.
 	// It is ignored in case Type is not "generic".
+	// Can make use of properties.
 	Generic string `json:"generic,omitempty" yaml:"generic,omitempty"`
 }
 
-// Map
-type Map map[string]*Type
+// MapTemplate
+type MapTemplate struct {
+	// Property, must be a stream
+	Property string `json:"property" yaml:"property"`
+	Index    string `json:"index" yaml:"index"`
+	Value    string `json:"value" yaml:"value"`
+	Expand   *Type  `json:"expand" yaml:"expand"`
+
+	Entry *Type `json:"entry" yaml:"entry"`
+}
 
 // Generics is a specification of generics.
 type Generics map[string]*Type
 
 // Resolves the type using the given provider.
-func (t Type) Resolve(typeProvider TypeProvider, generics Generics) (Type, error) {
+func (t Type) Resolve(typeProvider TypeProvider, generics Generics, values Values) (Type, error) {
 	resolved := Type{
 		Description: t.Description,
 		Type:        t.Type,
 	}
 
 	if t.Type == "stream" {
-		resolvedStream, err := t.Stream.Resolve(typeProvider, generics)
+		resolvedStream, err := t.Stream.Resolve(typeProvider, generics, values)
 		if err != nil {
 			return Type{}, err
 		}
 		resolved.Stream = &resolvedStream
 	} else if t.Type == "map" {
-		resolvedMap := make(map[string]*Type)
-		for sub, subtype := range t.Map {
-			resolvedSubtype, err := subtype.Resolve(typeProvider, generics)
+		resolvedMap := []*Type{}
+		for _, subtype := range t.Map {
+			resolvedSubtype, err := subtype.Resolve(typeProvider, generics, values)
 			if err != nil {
 				return Type{}, err
 			}
-			resolvedMap[sub] = &resolvedSubtype
+			resolvedMap = append(resolvedMap, &resolvedSubtype)
 		}
 		resolved.Map = resolvedMap
 	} else if t.Type == "generic" {
@@ -78,13 +105,13 @@ func (t Type) Resolve(typeProvider TypeProvider, generics Generics) (Type, error
 		}
 		refGens := make(Generics)
 		for gen, genType := range t.Generics {
-			resolvedGen, err := genType.Resolve(typeProvider, generics)
+			resolvedGen, err := genType.Resolve(typeProvider, generics, values)
 			if err != nil {
 				return Type{}, err
 			}
 			refGens[gen] = &resolvedGen
 		}
-		resolved, err = ref.Resolve(typeProvider, refGens)
+		resolved, err = ref.Resolve(typeProvider, refGens, values)
 		if err != nil {
 			return Type{}, err
 		}
@@ -97,16 +124,4 @@ func (t Type) Resolve(typeProvider TypeProvider, generics Generics) (Type, error
 	}
 
 	return resolved, nil
-}
-
-func (t Map) Resolve(typeProvider TypeProvider, generics Generics) (Map, error) {
-	newMap := make(Map)
-	for name, sub := range t {
-		newSub, err := sub.Resolve(typeProvider, generics)
-		if err != nil {
-			return nil, err
-		}
-		newMap[name] = &newSub
-	}
-	return newMap, nil
 }
